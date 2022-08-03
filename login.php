6c0cb0aceede99daa219d4531293be17c1d7aab4
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-08-02 22:16:45 -0300

    versao final

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 	<title>Login</title>
 </head>
 <body>
-	<form action="pagina_inicial.php" method="post">
+	<form action="pagina_verifica.php" method="post">
 		<h3>Faça seu login:</h3>
 		Usuário:  <input type="text" name="user"></p>
         Senha: 	 <input type="password" name="senha"></p>	   
